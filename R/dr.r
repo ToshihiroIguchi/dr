c94d7cfb89812b1121f0bb711ea5717a1214f4ed
ToshihiroIguchi <toshihiro.iguchi.mail@gmail.com>
2018-02-04 23:35:15 +0900

    add plot option

--- R/dr.r
+++ R/dr.r
@@ -25,21 +25,43 @@ dr <- function(formula, data, normalize = TRUE, unique = TRUE,
   #kpca.par はsigma,degree, scale, offset,orderも指定できる。
   #tsne.parのdimsは3以下がよい。
 
-  #入力をチェック
+  #入力をチェック---
   if(is.null(formula)){stop("formula is null.")}
   if(is.null(data)){stop("data is null.")}
-  if(length(formula) == 3){stop("You have mistyped the format of formula.")}
 
-  #データを準備
+  #データを準備---
   ret <- list() #戻り値
   ret$parameter$formula <- formula
   ret$parameter$normalize <- normalize
   ret$parameter$unique <- unique
 
   #解析用データ
-  data.original <- model.matrix(object = formula, data = data)[,-1] #解析用データ。[,-1]で切片を除く。
-  ret$parameter$data.head  <- data.original[1, ]
-  if(unique){data.original <- unique(data.original)} #ユニークなデータだけ抜粋。
+  if(length(formula) == 3){
+    data.original <- model.matrix(object = formula[-2], data = data)[,-1] #解析用データ。[,-1]で切片を除く。
+  }else{
+    data.original <- model.matrix(object = formula, data = data)[,-1] #解析用データ。[,-1]で切片を除く。
+  }
+
+  #ユニークなデータを抜粋
+  #https://qiita.com/weda_654/items/97c8dbba9f8198845537
+  u.no <- !duplicated(data.original)
+  data.original <- data.original[u.no,]
+
+  #目的変数がある場合の処理
+  if(length(formula) == 3){
+    y.formula  <- as.formula(paste0(as.character(formula[2]), "~+0"))
+    y.data <- model.frame(y.formula, data)[u.no,1]
+    ret$y <- y.data
+    formula[2] <- NULL #目的変数をさくじょ
+  }else{
+    ret$y <- NULL
+  }
+
+
+
+
+  #説明変数の項目名を抜粋
+  ret$parameter$names  <- names(data.original[1, ])
 
   #規格化。data.maが解析用の元データのマトリクス
   if(normalize){
@@ -66,10 +88,9 @@ dr <- function(formula, data, normalize = TRUE, unique = TRUE,
   if(!is.na(match("pca", methods))){
     cat("\n[Principal Components Analysis] \n")
     ret$pca <- prcomp(~., data = data.df,
-                      scale = normalize, center = normalize)
+                      scale = normalize, center = normalize,
+                      retx = TRUE)
   }
-
-
 
   #kernel PCA
   if(!is.na(match("kpca", methods))){
@@ -85,37 +106,45 @@ dr <- function(formula, data, normalize = TRUE, unique = TRUE,
 
 
   #MDS
-  cat("\n[Classical Multidimensional Scaling]\n")
-  ret$mds <- cmdscale(d = as.matrix(data.di), k = mds.par$k)
+  if(!is.na(match("mds", methods))){
+    cat("\n[Classical Multidimensional Scaling]\n")
+    ret$mds <- cmdscale(d = as.matrix(data.di), k = mds.par$k)
+  }
 
   #t-SNE
   #https://blog.albert2005.co.jp/2015/12/02/tsne/
   #注意点として、t-SNE で圧縮する次元は、 2・3 次元が推奨されています
-  cat("\n[t-Distributed Stochastic Neighbor Embedding]\n")
-  ret$tsne <- Rtsne(data.df,
-                    dims = tsne.par$dims, initial_dims = tsne.par$initial_dims,
-                    perplexity = tsne.par$perplexity, theta = tsne.par$theta)
+  if(!is.na(match("tsne", methods))){
+    cat("\n[t-Distributed Stochastic Neighbor Embedding]\n")
+    ret$tsne <- Rtsne(data.df,
+                      dims = tsne.par$dims, initial_dims = tsne.par$initial_dims,
+                      perplexity = tsne.par$perplexity, theta = tsne.par$theta)
+  }
 
   #LLE
   #m,kは再考の必要あり。
-  cat("\n[Locally linear embedding]\n")
-  ret$lle <- lle(data.ma, m = lle.par$m,
-                 k = lle.par$k,
-                 reg = lle.par$reg, p = lle.par$p)
+  if(!is.na(match("lle", methods))){
+    cat("\n[Locally linear embedding]\n")
+    ret$lle <- lle(data.ma, m = lle.par$m,
+                   k = lle.par$k,
+                   reg = lle.par$reg, p = lle.par$p)
+  }
 
   #SOM
   #その他のパラメータもあったほうがいい？
-  cat("\n[Self-Organizing Map]\n")
-  ret$som <- som(data.df, xdim=som.par$xdim, ydim=som.par$ydim)
+  if(!is.na(match("som", methods))){
+    cat("\n[Self-Organizing Map]\n")
+    ret$som <- som(data.df, xdim=som.par$xdim, ydim=som.par$ydim)
+  }
 
   #diffusion map
-  cat("\n[Diffusion Map]\n")
-  ret$diffmap <- diffuse(data.di,
-                         neigen = diffmap.par$neigen,
-                         t = diffmap.par$t, maxdim = diffmap.par$maxdim,
-                         delta = diffmap.par$delta)
-
-
+  if(!is.na(match("diffmap", methods))){
+    cat("\n[Diffusion Map]\n")
+    ret$diffmap <- diffuse(data.di,
+                           neigen = diffmap.par$neigen,
+                           t = diffmap.par$t, maxdim = diffmap.par$maxdim,
+                           delta = diffmap.par$delta)
+  }
 
 
   #戻り値
@@ -123,7 +152,73 @@ dr <- function(formula, data, normalize = TRUE, unique = TRUE,
   return(ret)
 }
 
-ret <- dr(~Sepal.Length+ Sepal.Width + Petal.Length+ Petal.Width+Species, data = iris)
+ret <- dr(Sepal.Length~Sepal.Length+ Sepal.Width + Petal.Length+ Petal.Width, data = iris)
+
+predict.dr <- function(result, data){
+  data.original <- model.matrix(object = result$parameter$formula, data = data)[,-1]
+
+  #モデルで使用した説明変数と、新データで作った説明変数が一致するか確認。
+  #カテゴリカル変数まわりでエラーが起きる気がする。
+  if(!setequal(result$parameter$names, names(data.original[1, ]))){
+    stop("Explanatory variable is different from model.")
+  }
+
+  #規格化。data.maが解析用の元データのマトリクス
+  if(result$parameter$normalize){
+    #規格化。もうちょっとスマートなのがある気がする…
+    data.ma <- t(apply(data.original,1,
+                       function(x){(x - result$parameter$data.mean)/result$parameter$data.sd}))
+  }else{
+    data.ma <- data.original
+  }
+
+  #変換
+  data.df <- data.frame(data.ma) #データフレームに変換
+
+  res <- list()
+
+  res$pca <- predict(result$pca, data.df)
+  res$kpca <- predict(result$kpca, data.df)
+
+
+  #res$tsne <- predict(result$tsne, data.df)
+  #https://stackoverflow.com/questions/43377941/t-sne-predictions-in-r
+  #t-SNEでpredictできないみたい。
+  #res$lle <- predict(result$lle, data.ma)
+  #res$som <- predict(result$som, data.df)
+}
+
+
+plot.dr <- function(result, cex = 1){
+  if(!is.null(result$y)){
+    if(is.factor(result$y)){
+      col_p <- rainbow(length(unique(result$y)))
+      plot_bg <- col_p[unclass(result$y)]
+    }else{
+      rank_y <- round(rank(result$y))
+      plot_bg <- heat.colors(length(result$y))[rank_y]
+    }
+  }else{
+    plot_bg <- NULL; col_p <- NULL
+  }
+
+  if(!is.null(result$pca)){
+    plot(result$pca$x[,1],result$pca$x[,2], xlab = "PC1", ylab = "PC2",
+         type = "p", pch = 21, cex = cex, bg = plot_bg)
+    #https://www1.doshisha.ac.jp/~mjin/R/Chap_07/07.html
+  }
+
+  if(!is.null(result$kpca)){
+    plot(result$kpca@pcv[,1],result$kpca@pcv[,2], xlab = "K-PC1", ylab = "K-PC2",
+         type = "p", pch = 21, cex = cex, bg = plot_bg)
+    #https://www1.doshisha.ac.jp/~mjin/R/Chap_07/07.html
+  }
+
+
+}
+
+plot(ret)
+
 
 
 
